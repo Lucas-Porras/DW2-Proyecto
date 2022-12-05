378958bdde8b06e90c0f859203a2fde51371f205
Lucas-Porras <abdhalalucas@gmail.com>
2022-12-05 19:12:15 -0300

    Terminamos

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mostrarImagen(){
+    document.getElementById('image').style.display="block";
+    document.getElementById('btnID').style.display = "none";
+    document.getElementById('lista').style.display="none";
+}
+function mostrarLista(){
+    document.getElementById('btnID2').style.display="block";
+}
